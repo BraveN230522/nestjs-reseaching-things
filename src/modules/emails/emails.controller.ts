e509b8b798fc5e771bf1297b69365b2fdee3ff64
Brave N <braven230522@gmail.com>
2023-01-28 13:20:48 +0700

    Update build hbs nestjs

--- src/modules/emails/emails.controller.ts
+++ src/modules/emails/emails.controller.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Controller } from '@nestjs/common';
 import { Post, UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
 import { EmailsService } from './emails.service';
 
 @Controller('emails')
@@ -9,6 +10,6 @@ export class EmailsController {
   constructor(private emailsService: EmailsService, private readonly httpService: HttpService) {}
   @Post()
   sendTemporaryAccessDeal() {
-    this.emailsService.sendTemporaryAccessDeal('abc', 'thanhhaluong2611@gmail.com');
+    return this.emailsService.sendTemporaryAccessDeal('abc', 'woodyntd@gmail.com');
   }
 }
